2eb09968f0a7f7a5a8c3ce48dda39e00ba1f650a
ToKra <kratas.tom@gmail.com>
2019-03-30 17:44:24 +0100

    add test file with first test

---
+++ test_bilateral_fusion_MLIC.py
@@ -0,0 +1,6 @@
+import pytest
+
+from bilateral_fusion_MLIC import Bilateral_fusion_MLIC
+
+def test_Bilateral_fusion_MLIC_can_be_instantiated():
+    fusion_class = Bilateral_fusion_MLIC(None, None, None, None, None)
