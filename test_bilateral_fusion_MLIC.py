460812d3746242fc340b657050ed5e4f1c6e38af
ToKra <kratas.tom@gmail.com>
2019-03-31 01:16:55 +0100

    add fuse method test

--- test_bilateral_fusion_MLIC.py
+++ test_bilateral_fusion_MLIC.py
@@ -1,6 +1,13 @@
 import pytest
+import cv2
 
 from bilateral_fusion_MLIC import Bilateral_fusion_MLIC
 
 def test_Bilateral_fusion_MLIC_can_be_instantiated():
-    fusion_class = Bilateral_fusion_MLIC(None, None, None, None, None)
+    MLIC_fusion = Bilateral_fusion_MLIC(image_set=None, kernel_size=None, scale_depth=None, alpha=None, beta=None)
+
+def test_fuse_method_works_with_ok_input():
+    test_image = cv2.imread('test_images/lena.png', 1)
+    test_image_set = [test_image]
+    
+    MLIC_fusion = Bilateral_fusion_MLIC(image_set=test_image_set, kernel_size=5, scale_depth=2, alpha=None, beta=None)
