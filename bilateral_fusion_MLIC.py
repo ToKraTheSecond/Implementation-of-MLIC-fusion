5258a4c965be5172bb3b2b4aea31a2498f82dadf
ToKra <kratas.tom@gmail.com>
2019-04-07 19:57:56 +0200

    correct indexing errors; use class instance variables; add call to fuse()

--- bilateral_fusion_MLIC.py
+++ bilateral_fusion_MLIC.py
@@ -9,6 +9,9 @@ class Bilateral_fusion_MLIC:
         self.alpha = alpha
         self.beta = beta
         self.converted_image_set = []
+        self.decomposed_set = []
+        self.range_gaussian = 0
+        self.spatial_gaussian = 0
 
     def convert_color_space(self, target_space):
         if target_space == 'RGB2YUV':
@@ -16,7 +19,7 @@ class Bilateral_fusion_MLIC:
         elif target_space == 'YUV2RGB':
             self.converted_image_set = [cv2.cvtColor(image, cv2.COLOR_YUV2RGB) for image in self.image_set]
 
-        return converted_image_set
+        return self.converted_image_set
   
     def log_y_channels(self):
         #Y channel has index 0 - [Y, U, V]
@@ -24,7 +27,7 @@ class Bilateral_fusion_MLIC:
             
         return log_y_channel_set
 
-    def get_intensity_range(y_channel):
+    def get_intensity_range(self, y_channel):
         """Needed for setting range gaussian value."""
         # TODO:
         # Research how to do this
@@ -32,28 +35,37 @@ class Bilateral_fusion_MLIC:
 
         pass
 
-    def apply_decomposition_step(self, image, spatial_gaussian, range_gaussian):
+    def apply_decomposition_step(self, image):
         rows_count, columns_count = image.shape
         output_image = np.zeros((rows_count, columns_count))
 
-        # so we do not need to manually iterate over each pixel in kernel area
+        # spatial_response won't change during iterations so we can precalculate it
         xx, yy = np.meshgrid(range(-self.kernel_size, self.kernel_size + 1), 
                              range(-self.kernel_size, self.kernel_size + 1))
 
-        spatial_response = np.exp( - (xx ** 2 + yy ** 2) / (spatial_gaussian ** 2))
+        spatial_response = np.exp( - (xx ** 2 + yy ** 2) / (self.spatial_gaussian ** 2))
 
-        for row in rows:
-            for column in columns:
+        for row in range(rows_count):
+            for column in range(columns_count):
                 # stay inside input image indexing space
-                row_min = max(row - self.kernel_size, 1)
-                row_max = min(row + self.kernel_size, rows_count)
-                column_min = max(column - self.kernel_size, 1)
-                column_max = min(column + self.kernel_size, columns_count)
-                roi = image[row_min:row_max + 1, column_min, column_max + 1]
+                row_min = max(row - self.kernel_size, 0)
+                row_max = min(row + self.kernel_size, rows_count - 1)
+                column_min = max(column - self.kernel_size, 0)
+                column_max = min(column + self.kernel_size, columns_count - 1)
+                roi = image[row_min:row_max + 1, column_min:column_max + 1]
 
-                range_response = np.exp(-1 * ((roi - image[row, column]) ** 2 / (range_gaussian ** 2)))
+                # spatial_response offsets -> only correct spatial_response region will be indexed
+                r_o = -1 * row + self.kernel_size
+                c_o = -1 * column + self.kernel_size
+                spatial_response_off = spatial_response[row_min + r_o:row_max + r_o + 1,
+                                                       column_min + c_o:column_max + c_o + 1]
 
-                output_image[row, column] = (1 / np.sum(responses_product)) * (spatial_response * range_response * roi)
+                range_response = np.exp(-1 * ((roi - image[row, column]) ** 2 / (self.range_gaussian ** 2)))                
+                responses_product = spatial_response_off * range_response
+
+
+
+                output_image[row, column] = (1 / np.sum(responses_product)) * np.sum((responses_product * roi))
        
         return output_image
     
@@ -65,13 +77,16 @@ class Bilateral_fusion_MLIC:
         if self.scale_depth > 1:
             for scale_step in range(1, self.scale_depth + 1):
                 # TODO: Only first try, should be further evaluated
-                spatial_gaussian = 2 ** (scale_step - 1)
-                range_gaussian = 0.1 / (2 ** (scale_step - 1))
+                self.spatial_gaussian = 2 ** (scale_step - 1)
+                self.range_gaussian = 0.1 / (2 ** (scale_step - 1))
             
-                decomposed_images.append(apply_decomposition_step(self, image, spatial_gaussian, range_gaussian))
+                decomposed_images.append(self.apply_decomposition_step(image))
 
         return decomposed_images
 
     def fuse(self):
-        convert_color_space('RGB2YUV')
-        log_y_channels()
+        self.convert_color_space('RGB2YUV')
+        self.log_y_channel_set = self.log_y_channels()
+        self.decomposed_set = [self.apply_decomposition(image) for image in self.log_y_channel_set]
+
+        return self.decomposed_set
