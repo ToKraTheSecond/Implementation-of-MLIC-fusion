7cd2c89cbc494d07222a0cc6fb80e983ba2c7814
ToKra <kratas.tom@gmail.com>
2019-04-09 19:18:20 +0200

    add test_decomposed_set_contains_correct_number_of_image()

--- test_bilateral_fusion_MLIC.py
+++ test_bilateral_fusion_MLIC.py
@@ -7,8 +7,22 @@ def test_Bilateral_fusion_MLIC_can_be_instantiated():
     MLIC_fusion = Bilateral_fusion_MLIC(image_set=None, kernel_size=None, scale_depth=None, alpha=None, beta=None)
 
 def test_fuse_method_works_with_ok_input():
-    test_image = cv2.imread('test_images/lena_roi.png', 1)
+    test_image = load_test_image()
     test_image_set = [test_image]
     
     MLIC_fusion = Bilateral_fusion_MLIC(image_set=test_image_set, kernel_size=3, scale_depth=2, alpha=None, beta=None)
     MLIC_fusion.fuse()
+
+def test_decomposed_set_contains_correct_number_of_image():
+    test_image = load_test_image()
+    test_image_set = [test_image]
+
+    MLIC_fusion = Bilateral_fusion_MLIC(image_set=test_image_set, kernel_size=3, scale_depth=2, alpha=None, beta=None)
+    MLIC_fusion.fuse()
+    
+    assert len(MLIC_fusion.decomposed_set[0]) == 2
+
+def load_test_image():
+    test_image = cv2.imread('test_images/lena_roi.png', 1)
+
+    return test_image
