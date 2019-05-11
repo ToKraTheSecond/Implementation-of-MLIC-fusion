cf0cd9b10b348440e0057899bb41e0b03d34ac2e
ToKra <kratas.tom@gmail.com>
2019-05-11 20:16:34 +0200

    add pipeline test with multiple images in input set; load test image as float32

--- test_bilateral_fusion_MLIC.py
+++ test_bilateral_fusion_MLIC.py
@@ -7,11 +7,18 @@ from bilateral_fusion_MLIC import Bilateral_fusion_MLIC
 def test_Bilateral_fusion_MLIC_can_be_instantiated():
     MLIC_fusion = Bilateral_fusion_MLIC(image_set=None, kernel_size=None, scale_depth=None, alpha=None, beta=0.8)
 
-def test_fuse_method_works_with_ok_input():
+def test_fuse_method_works_with_one_ok_input():
     test_image = load_test_image()
     test_image_set = [test_image]
     
-    MLIC_fusion = Bilateral_fusion_MLIC(image_set=test_image_set, kernel_size=3, scale_depth=2, alpha=None, beta=0.8)
+    MLIC_fusion = Bilateral_fusion_MLIC(image_set=test_image_set, kernel_size=3, scale_depth=3, alpha=None, beta=0.8)
+    MLIC_fusion.fuse()
+
+def test_fuse_method_works_with_multiple_ok_inputs():
+    test_image = load_test_image()
+    test_image_set = [test_image, test_image, test_image]
+
+    MLIC_fusion = Bilateral_fusion_MLIC(image_set=test_image_set, kernel_size=3, scale_depth=3, alpha=None, beta=0.8)
     MLIC_fusion.fuse()
 
 @pytest.mark.parametrize('scale_depth, expected', [
@@ -46,4 +53,4 @@ def test_first_image_in_decomposed_image_set_is_unchanged():
 def load_test_image():
     test_image = cv2.imread('test_images/lena_roi.png', 1)
 
-    return test_image
+    return test_image.astype(np.float32)
